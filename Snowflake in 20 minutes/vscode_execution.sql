8607a34bb0ae2cf70a1c7791426a86b2381d4a8e
PratikDavidson <mcknight.m134@gmail.com>
2024-08-02 16:28:22 +0530

    Added complete code with //comments

--- Snowflake in 20 minutes/vscode_execution.sql
+++ Snowflake in 20 minutes/vscode_execution.sql
@@ -1,13 +1,14 @@
-//Snowflake in 20 minutes
-
+//Step-1: Create virtual warehouse
 CREATE OR REPLACE WAREHOUSE sf_tuts_wh WITH
     WAREHOUSE_SIZE = 'X-SMALL'
     AUTO_SUSPEND = 180
     AUTO_RESUME = TRUE
     INITIALLY_SUSPENDED = TRUE;
 
+//Step-2: Create database with default schema
 CREATE OR REPLACE DATABASE sf_tuts;
 
+//Step-3: Create table
 CREATE OR REPLACE TABLE emp_basic (
     first_name STRING,
     last_name STRING,
@@ -17,4 +18,17 @@ CREATE OR REPLACE TABLE emp_basic (
     start_date DATE
 );
 
-PUT file://D://Snowflake//'Snowflake in 20 minutes'//employees0*.csv @sf_tuts.public.%emp_basic;
+//Step-4: Stage data files
+PUT 'file://D:/Snowflake/Snowflake in 20 minutes/employees0*.csv' @sf_tuts.public.%emp_basic;
+
+LIST @sf_tuts.public.%emp_basic;
+
+//Step-5: Copy data into target tables
+COPY INTO emp_basic
+  FROM @%emp_basic
+  FILE_FORMAT = (type = csv field_optionally_enclosed_by='"')
+  PATTERN = '.*employees0[1-5].csv.gz'
+  ON_ERROR = 'skip_file';
+
+//Step-6: Query loaded data
+SELECT * FROM emp_basic;
